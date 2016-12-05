55fb7ff676d524f954fd3d4f8b3c1d2da4e40276
Siameseguy <psyhakhom@gmail.com>
2016-12-05 13:30:18 -0500

    added nav menu

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,4 +1,7 @@
 class SplashController < ApplicationController
   def home
   end
+
+  def previous
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'splash#home'
+  get '/previous', to: 'splash#previous', as: 'previous'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
